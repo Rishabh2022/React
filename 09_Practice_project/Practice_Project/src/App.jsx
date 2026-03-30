a3d0d25c9e0a4db937afbc83008f1cf74b55ba5b
Rishabh <rishabhhjain1@gmail.com>
2026-03-30 09:45:42 +0530

    Practice project 09

---
+++ 09_Practice_project/Practice_Project/src/App.jsx
@@ -0,0 +1,11 @@
+import { ProjectsSideBar } from "./components/ProjectsSideBar"; 
+
+function App() {
+  return (
+    <main className="h-screen my-8">
+      <ProjectsSideBar></ProjectsSideBar>
+    </main>
+  );
+}
+
+export default App;
